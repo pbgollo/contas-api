ca3f150c0442635e347defcef90afa5ca48b4e26
Pedro Gollo <pedrobgollo@gmail.com>
2024-06-06 13:01:08 -0300

    Primeira migração

---
+++ Migrations/20240606155909_InitialCreate.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ContasAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Contas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Nome = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    ValorOriginal = table.Column<decimal>(type: "decimal(65,30)", nullable: false),
+                    DataVencimento = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    DataPagamento = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    ValorCorrigido = table.Column<decimal>(type: "decimal(65,30)", nullable: false),
+                    DiasAtraso = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Contas", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Contas");
+        }
+    }
+}
