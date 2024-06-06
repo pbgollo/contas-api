f516399908c450f2ec0fe698917433bcc326d36e
Pedro Gollo <pedrobgollo@gmail.com>
2024-06-06 13:30:16 -0300

    Adição das regras de negócio no contexto do banco

--- Data/DataBaseContext.cs
+++ Data/DataBaseContext.cs
@@ -5,5 +5,6 @@ namespace ContasAPI.Data {
     public class DataBaseContext : DbContext {
         public DataBaseContext(DbContextOptions<DataBaseContext> options) : base(options) { }
         public DbSet<Conta> Contas { get; set; }
+        public DbSet<RegraNegocio> RegrasNegocio { get; set; }
     }
 }
