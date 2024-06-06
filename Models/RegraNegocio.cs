79dc0db8b2fcfedc5f9f0b4d1ea6335479e86508
Pedro Gollo <pedrobgollo@gmail.com>
2024-06-06 13:16:07 -0300

    Criação do modelo das regras de negócio

---
+++ Models/RegraNegocio.cs
@@ -0,0 +1,6 @@
+public class RegraNegocio {
+    public int Id { get; set; }
+    public int DiasEmAtraso { get; set; }
+    public decimal Multa { get; set; }
+    public decimal JurosPorDia { get; set; }
+}
